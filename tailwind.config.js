e8d1c95ee17cc499dc9399a7df3bc9b94f0935ea
Igor <Igorcbraz1@gmail.com>
2022-12-26 16:19:51 -0300

    Chore: Add eslint and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#CEE4B6',
+          200: '#ADC295',
+          300: '#8DA276',
+          400: '#6E8258',
+          500: '#50643c',
+        },
+        secondary: {
+          100: '#F3DCD3',
+          200: '#D5BFB6',
+          300: '#B8A29A',
+          400: '#9C877E',
+          500: '#806C64',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
