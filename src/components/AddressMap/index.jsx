48fbea434ef6f41a8d1676396620b818da3c5762
Igor <Igorcbraz1@gmail.com>
2022-12-27 15:52:38 -0300

    FEAT: Section to landing page
    Add localization,  hours of operation and google maps embed code  to new section

---
+++ src/components/AddressMap/index.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+export const AddressMap = ({ className = 'w-full h-full' }) => {
+  return (
+    <iframe
+      src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.0478203500575!2d-46.86826018509086!3d-23.745674074309456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cfb27e5a744885%3A0x333b13f0aa6db8b5!2sTemplo%20Enkoji!5e0!3m2!1spt-BR!2sbr!4v1672166189508!5m2!1spt-BR!2sbr'
+      className={className}
+      style={{ border:0 }}
+      allowFullScreen=''
+      loading='lazy'
+      referrerPolicy='no-referrer-when-downgrade'
+    />
+  )
+}
+
+AddressMap.propTypes = {
+  className: PropTypes.string,
+}
