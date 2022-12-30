f91a8fd2dcc063ed845707f03e57aecb7e2a2c11
Igor <Igorcbraz1@gmail.com>
2022-12-29 21:13:01 -0300

    FEAT: Starting using flowbite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,6 +32,8 @@ import Youtube from '../assets/icons/youtube.svg';
 import { ReactComponent as Waze } from '../assets/icons/waze.svg';
 import { ReactComponent as Maps } from '../assets/icons/maps.svg';
 
+import 'flowbite';
+
 const Links = {
   socialMedia: {
     instagram: 'https://www.instagram.com/temploenkoji/',
@@ -88,7 +90,7 @@ export const Home = () => {
         </div>
       </header>
       <nav className='flex justify-center bg-primary-500 pb-6 h-4/5'>
-        <div className='flex items-center gap-2 w-4/5'>
+        <div className='flex items-center gap-2 w-4/5' id='navbar-default'>
           <img
             src={Logo}
             alt='Logo templo enkoji'
@@ -133,6 +135,10 @@ export const Home = () => {
             </li>
           </ul>
         </div>
+        <button data-collapse-toggle='navbar-default' type='button' className='inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden focus:outline-none' aria-controls='navbar-default' aria-expanded='false'>
+          <span className='sr-only'>Open main menu</span>
+          <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clipRule='evenodd'></path></svg>
+        </button>
       </nav>
       <section className='flex justify-center pb-5 bg-primary-500'>
         <div className='flex justify-between gap-4 w-3/4 border border-white text-white p-9'>
