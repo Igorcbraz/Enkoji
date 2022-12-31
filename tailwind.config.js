7ade37f96be1a9ee3532f8e143fc4164faa70388
Igor <Igorcbraz1@gmail.com>
2022-12-30 23:02:58 -0300

    build: Remove flowbite ⚙

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/flowbite/**/*.js'
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
@@ -36,6 +35,5 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin')
   ]
 }
