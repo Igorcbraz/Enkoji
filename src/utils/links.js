68a821dc5e71abb585b1477fd0b8771efd14029e
Igor <Igorcbraz1@gmail.com>
2023-03-12 11:57:13 -0300

    :recycle: Change links

--- src/utils/links.js
+++ src/utils/links.js
@@ -4,11 +4,11 @@ export const Links = {
     facebook: 'https://www.facebook.com/Templo-Enkoji-702093283924657',
     whatsapp: 'https://wa.me/5555119135296?text=Ol%C3%A1%2C+gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+o+Templo+Enkoji+%F0%9F%91%8B%F0%9F%98%81',
     youtube: ' https://www.youtube.com/@enkojitemplo',
-    maps: 'https://www.google.com.br/travel/entity/key/ChgIxa_Dhqec3hMaDS9nLzExZzZ5bWxtcTAQBA/reviews?ts=CAESABoECgIaACoECgAaAA&utm_campaign=sharing&utm_medium=link_btn&utm_source=poi',
+    maps: 'https://g.page/r/CbW4barwEzszEBM/review',
     shop: 'https://www.enkoji.com.br/'
   },
   track: {
     waze: 'https://ul.waze.com/ul?place=ChIJhUh0Wn6yz5QRtbhtqvATOzM&ll=-23.74567900%2C-46.86607150&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location',
-    maps: 'https://www.google.com.br/travel/entity/key/ChgIxa_Dhqec3hMaDS9nLzExZzZ5bWxtcTAQBA?ved=0CAAQ5JsGahcKEwiwrrXLjp_8AhUAAAAAHQAAAAAQBQ&ts=CAESABoECgIaACoECgAaAA&utm_campaign=sharing&utm_medium=link_btn&utm_source=poi'
+    maps: 'https://g.page/r/CbW4barwEzszEBM/review'
   }
 }
